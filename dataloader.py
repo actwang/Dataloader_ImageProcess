0db1385798485530b36c6af62b5fa13040f1ae0b
actwang <actonnwang@gmail.com>
2019-10-31 00:26:44 -0400

    updated model

--- dataloader.py
+++ dataloader.py
@@ -11,6 +11,7 @@ import time
 import os
 import copy
 from torchvision import datasets, models, transforms
+from .LeNet import LeNet5
 from sklearn.externals import joblib
 from PIL import Image
 from pathlib import Path
@@ -18,13 +19,43 @@ from pathlib import Path
 # Image size required by your neural network
 HEIGHT = 256
 WIDTH = 256
+patch_dir = '/home/zi29/Desktop/IMP/wk4/dataset/raw/patches'
+patch_set = ['train', 'test', 'val']
+patch_lab = ['dpi75', 'dpi150', 'dpi300', 'dpi600']
+# for state in patch_set:
+#     for label in patch_lab:
+#         img_path = '/'.join((patch_dir, state, label))
+#         loader = DataLoader(img_path, label, batch_size=4)
+#         print(loader.batch_size)
+#         print(loader.num_batches)
+
+# N_batch = 3
+# for i in range(N_batch):
+#     showABatch(trainLoader.get_batch('train'))
+#     showABatch(testLoader.get_batch('test'))
+#     showABatch(valLoader.get_batch('val'))
+data_transforms = {
+        'train': transforms.Compose([
+            transforms.CenterCrop(128),
+            transforms.ToTensor()
+            ]),
+        'val': transforms.Compose([
+            transforms.ToTensor()
+            ]),
+        'test': transforms.Compose([
+            transforms.ToTensor()
+        ])
+}
 
 
 class DataLoader(object):
 
-    def __init__(self, pathToImage, label, batch_size=4):
+    def __init__(self, path_to_labels, batch_size=4):
         # reading data list
-        self.list_img = [k.split('/')[-1].split('.')[0] for k in glob.glob(os.path.join(pathToImage, '*.tif'))]
+        # now it's a list of all images in that dataset(train or val)
+        self.list_img = []
+        for y in range(4):
+            self.list_img.append((k.split('/')[-1].split('.')[0] for k in glob.glob(os.path.join(path_to_labels, patch_lab[y], '*.tif'))))
         # store the batch size
         self.batch_size = batch_size
         # store the total number of images
@@ -34,9 +65,7 @@ class DataLoader(object):
         # store the number of batches
         self.num_batches = self.size // batch_size
         # store image path
-        self.path = pathToImage
-        # label as parent directory
-        self.label = label
+        self.path = path_to_labels
 
     def get_batch(self, state):
         # once we reach the end of the dataset, shuffle it again and reset cursor
@@ -48,39 +77,23 @@ class DataLoader(object):
         # initialize the label tensor with zeros, 3 here is the size of one-hot encoded label for a 3-class classification problem
         labels = torch.zeros(self.batch_size, 4)
 
+        # get_batch() still not random. Takes one from each label per batch
         for idx in range(self.batch_size):
             # get the current file name pointed by the cursor
             curr_file = self.list_img[self.cursor]
             print('cursor' + str(self.cursor))
-            # get the full path to that image
-            full_img_path = os.path.join(self.path, state, self.label, curr_file + '.tif')
+            # get the full path to that image?????????????
+            full_img_path = os.path.join(patch_dir, state, patch_lab[idx], curr_file+'.tif')
             # update cursor
             self.cursor += 1
-
-            temp_dict = {0: 'dpi600', 1: 'dpi300', 2: 'dpi150', 3: 'dpi75'}
 
             # read image in grayscale
             image = cv2.imread(full_img_path, 0)
             imgs = image.data_transforms[state]
 
-            # # randomly resize an image to 600, 500 or 400 dpi and resize it back to 600dpi
-            # temp_dict = {0: 600, 1: 500, 2: 400}
-            # rand_int = np.random.randint(3)
-            # if rand_int != 0:
-            #     resized_img = cv2.resize(image, None, fx=temp_dict[rand_int]/600, fy=temp_dict[rand_int]/600,
-            #                              interpolation=cv2.INTER_AREA)
-            #     resized_img = cv2.resize(resized_img, None, fx=600/temp_dict[rand_int], fy=600/temp_dict[rand_int],
-            #                              interpolation=cv2.INTER_LINEAR)
-            # else:
-            #     resized_img = image
-
-            # augumentation
-            # imgs[idx,0,:,:] = to_tensor(Image.fromarray(resized_img))
-            #
-
             # label index
-            # index() will work only in this case because the keys are also the index in the dictionary
-            lab_ind = list(temp_dict.values()).index(self.label)
+            # Here is where we use split to find out what label it belongs to
+            lab_ind = # How to get the labeling information???
             labels[idx][lab_ind] = 1
 
         return imgs, labels
@@ -110,134 +123,3 @@ def showABatch(batch, title=None):
     plt.show()
 
 
-# visualize your results
-# save to directories
-patch_dir = '/home/zi29/Desktop/IMP/wk4/dataset/raw/patches'
-patch_set = ['train', 'test', 'val']
-patch_lab = ['dpi75', 'dpi150', 'dpi300', 'dpi600']
-
-# for state in patch_set:
-#     for label in patch_lab:
-#         img_path = '/'.join((patch_dir, state, label))
-#         loader = DataLoader(img_path, label, batch_size=4)
-#         print(loader.batch_size)
-#         print(loader.num_batches)
-
-# N_batch = 3
-# for i in range(N_batch):
-#     showABatch(trainLoader.get_batch('train'))
-#     showABatch(testLoader.get_batch('test'))
-#     showABatch(valLoader.get_batch('val'))
-
-# Qu: where does this go?
-data_transforms = {
-        'train': transforms.Compose([
-            transforms.CenterCrop(128),
-            transforms.ToTensor()
-            ]),
-        'val': transforms.Compose([
-            transforms.ToTensor()
-            ]),
-        'test': transforms.Compose([
-            transforms.ToTensor()
-        ])
-}
-# join data_dir and x = 'train' and 'val' for directory as input along with the transform methods.
-# pass them to ImageFolder and so it's a dictionary with keys 'train' and 'val'
-
-
-# EDIT GET_BATCH()
-def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
-    since = time.time()
-
-    best_model_wts = copy.deepcopy(model.state_dict())  # changing best_model_wts doesn't change model
-    best_acc = 0.0
-
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))  # format substitutes inputs into {}
-        print('-' * 10)
-
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'val', 'test']:
-            if phase == 'train':
-                model.train()  # Set model to training mode
-            else:
-                model.eval()   # Set model to evaluate mode
-
-            running_loss = 0.0
-            running_corrects = 0
-
-            # Iterate over data.
-            for inputs, labels in DataLoader.get_batch(phase):
-                inputs = inputs.to(device)
-                labels = labels.to(device)
-
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs = model(inputs)
-                    _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
-
-                    # backward + optimize only if in training phase
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
-
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-            if phase == 'train':
-                scheduler.step()
-
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects.double() / dataset_sizes[phase]
-
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
-
-            # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = copy.deepcopy(model.state_dict())
-
-        print()
-
-    time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
-
-    # load best model weights
-    model.load_state_dict(best_model_wts)
-    return model
-
-
-# image_datasets = {x: datasets.ImageFolder(os.path.join(patch_dir, x), data_transforms[x])
-#                for x in ['train', 'val']}
-
-
-dataloaders = {x: DataLoader(patch_dir, x, batch_size=4)
-               for x in ['train', 'val']}
-
-# don't need class names
-dataset_sizes = {x: len(dataloaders[x]) for x in ['train', 'val']}
-# class_names = image_datasets['train'].classes
-
-print(str(torch.cuda.is_available()))
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-# LeNet take input 128x128
-model_or = models.resnet18(pretrained=False)
-num_ftrs = model_or.fc.in_features
-model_or.fc = nn.Linear(num_ftrs, 4)
-model_or = model_or.to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer_or = optim.SGD(model_or.parameters(), lr=0.001, momentum=0.9)
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_or, step_size=7, gamma=0.1)
-
-model_or = train_model(model_or, criterion, optimizer_or, exp_lr_scheduler, num_epochs=25)
---
+++ model_or.py
@@ -0,0 +1,123 @@
+from __future__ import print_function, division
+import glob
+import numpy as np
+import cv2
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+from torchvision import datasets, models, transforms
+from .LeNet import LeNet5
+from .dataloader import DataLoader
+
+data_transforms = {
+        'train': transforms.Compose([
+            transforms.CenterCrop(128),
+            transforms.ToTensor()
+            ]),
+        'val': transforms.Compose([
+            transforms.ToTensor()
+            ]),
+        'test': transforms.Compose([
+            transforms.ToTensor()
+        ])
+}
+# join data_dir and x = 'train' and 'val' for directory as input along with the transform methods.
+# pass them to ImageFolder and so it's a dictionary with keys 'train' and 'val'
+
+
+# EDIT GET_BATCH()
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())  # changing best_model_wts doesn't change model
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))  # format substitutes inputs into {}
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val','test']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in DataLoader.get_batch(model, phase):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+patch_dir = '/home/zi29/Desktop/IMP/wk4/dataset/raw/patches'
+patch_set = ['train', 'test', 'val']
+patch_lab = ['dpi75', 'dpi150', 'dpi300', 'dpi600']
+
+# trying to get train: all train images and val: all val images
+dataloaders = {x: DataLoader(os.path.join(patch_dir, x), batch_size=4)
+               for x in ['train', 'val']}
+
+dataset_sizes = {x: len(dataloaders[x]) for x in ['train', 'val']}
+print(str(torch.cuda.is_available()))
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# LeNet take input 128x128
+model_or = LeNet5()
+model_or = model_or.to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer_or = optim.SGD(model_or.parameters(), lr=0.1, momentum=0.9)
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_or, step_size=7, gamma=0.1)
+
+model_or = train_model(model_or, criterion, optimizer_or, exp_lr_scheduler, num_epochs=25)
